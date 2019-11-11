1c6b765c3c5f07b856178f7465884ca6af497f70
Ben Pinner <ben.pinner@iress.com>
2019-11-11 10:44:23 +0000

    first commit

---
+++ Even_Fibonacci_Numbers/Even_Fibonacci_Numbers/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Even_Fibonacci_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Fibonnaci();
+
+        }
+
+        static List<int> Fibonnaci()
+        {
+            List<int> fib = new List<int>() { 1, 2 };
+
+            var last = fib.Last();
+            var next = fib[fib.Count - 2];
+            int hold;
+            while (next < 4000000)
+            {
+                hold = next;
+                next += last;
+                last = hold;
+
+                fib.Add(next);
+            }
+            foreach(int num in fib)
+            {
+                Console.WriteLine(num);
+            }
+            Console.ReadLine();
+            return new List<int>() { 0 };
+        }
+    }
+}
---
+++ Multiples_Of_3_And_5/Multiples_Of_3_And_5/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiples_Of_3_And_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Count() == 0)
+            {
+                Console.WriteLine("Argument Required: integer");
+            }
+            else
+            {
+                var numbers = new List<int>();
+
+                int number = Int32.Parse(args[0]);
+                for(int i = 0; i < number; i++)
+                {
+                    Console.WriteLine(i);
+                    if (i % 3 == 0 || i % 5 == 0)
+                    {
+                        numbers.Add(i);
+                    }
+                }
+                Console.WriteLine(numbers.AsQueryable().Sum());
+                Console.ReadLine();
+            }
+        }
+    }
+}
